c64aa7f53da84b73af61ca6b7bc383cb810016cf
sergo <sergokov@gmail.com>
2017-10-02 19:12:36 +0300

    SVM init commit.

---
+++ assignment1/svm.py
@@ -0,0 +1,6 @@
+import random
+import numpy as np
+from utils import load_CIFAR10
+import matplotlib.pyplot as plt
+
+train_data, train_labels, test_data, test_labels = load_CIFAR10('/home/sergo/Work/ML_Experiments/cifar-10-batches-py/')
