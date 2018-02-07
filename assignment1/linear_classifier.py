3c750f8383cc5fc2eac4297ccc0dee5726dad4b5
sergo <sergokov@gmail.com>
2018-02-07 23:57:48 +0300

    Softmax classifier.

--- assignment1/linear_classifier.py
+++ assignment1/linear_classifier.py
@@ -52,10 +52,22 @@ class LinearSVM(LinearClassifier):
       return loss, dW
 
 
-
 class Softmax(LinearClassifier):
 
-  def loass(self, X, y, reg):
-      num_train = X.shape[0]
+  def loss(self, X, y, reg):
+      loss = 0.0
+      dW = np.zeros_like(self.W)
       num_classes = self.W.shape[1]
+      num_train = X.shape[0]
       scores = X.dot(self.W)
+      shift_scores = scores - np.max(scores, axis=1).reshape(-1, 1)
+      softmax_output = np.exp(shift_scores) / np.sum(np.exp(shift_scores), axis=1).reshape(-1, 1)
+      loss = -np.sum(np.log(softmax_output[range(num_train), list(y)]))
+      loss /= num_train
+      loss += 0.5 * reg * np.sum(self.W * self.W)
+      dS = softmax_output.copy()
+      dS[range(num_train), list(y)] += -1
+      dW = (X.T).dot(dS)
+      dW = dW / num_train + reg * self.W
+
+      return loss, dW
--- assignment1/softmax.py
+++ assignment1/softmax.py
@@ -4,7 +4,7 @@ import time
 import matplotlib.pyplot as plt
 
 from utils import load_CIFAR10
-from linear_classifier import LinearSVM
+from linear_classifier import Softmax
 
 if __name__ == '__main__':
     train_data, train_labels, val_data, val_labels = \
@@ -19,9 +19,9 @@ if __name__ == '__main__':
     dev_labels = train_labels[mask]
 
     mean_image = np.mean(train_data, axis=0)
-    plt.figure(figsize=(4, 4))
-    plt.imshow(mean_image.reshape((32, 32, 3)).astype('uint8'))
-    plt.show()
+    # plt.figure(figsize=(4, 4))
+    # plt.imshow(mean_image.reshape((32, 32, 3)).astype('uint8'))
+    # plt.show()
 
     train_data -= mean_image
     val_data -= mean_image
@@ -30,3 +30,67 @@ if __name__ == '__main__':
     train_data = np.hstack([train_data, np.ones((train_data.shape[0], 1))])
     val_data = np.hstack([val_data, np.ones((val_data.shape[0], 1))])
     dev_data = np.hstack([dev_data, np.ones((dev_data.shape[0], 1))])
+
+    learning_rates = [1.4e-7, 1.5e-7, 1.6e-7]
+    range = range(-3, 3)
+    regularization_strengths = [(1 + i * 0.1) * 1e4 for i in range] + [(2 + 0.1 * i) * 1e4 for i in range]
+
+    results = {}
+    best_val = -1
+    best_svm = None
+
+    for rs in regularization_strengths:
+        for lr in learning_rates:
+            sofmax = Softmax()
+            loss_hist = sofmax.train(train_data, train_labels, lr, rs, num_iters=3000)
+            train_labels_pred = sofmax.predict(train_data)
+            train_accuracy = np.mean(train_labels == train_labels_pred)
+            val_labels_pred = sofmax.predict(val_data)
+            val_accuracy = np.mean(val_labels == val_labels_pred)
+            if val_accuracy > best_val:
+                best_val = val_accuracy
+                best_svm = sofmax
+            results[(lr, rs)] = train_accuracy, val_accuracy
+            print 'lr %e reg %e train accuracy: %f val accuracy: %f' % (lr, rs, train_accuracy, val_accuracy)
+
+    for lr, reg in sorted(results):
+        train_accuracy, val_accuracy = results[(lr, reg)]
+        print 'lr %e reg %e train accuracy: %f val accuracy: %f' % (lr, reg, train_accuracy, val_accuracy)
+
+    print 'Best cross-validation accuracy: %f' % best_val
+
+    import math
+
+    x_scatter = [math.log10(x[0]) for x in results]
+    y_scatter = [math.log10(x[1]) for x in results]
+
+    marker_size = 100
+    colors = [results[x][0] for x in results]
+    plt.subplot(2, 1, 1)
+    plt.scatter(x_scatter, y_scatter, marker_size, c=colors)
+    plt.colorbar()
+    plt.xlabel('log learning rate')
+    plt.ylabel('log regularization strength')
+    plt.title('CIFAR-10 training accuracy')
+
+    colors = [results[x][1] for x in results]  # default size of markers is 20
+    plt.subplot(2, 1, 2)
+    plt.scatter(x_scatter, y_scatter, marker_size, c=colors)
+    plt.colorbar()
+    plt.xlabel('log learning rate')
+    plt.ylabel('log regularization strength')
+    plt.title('CIFAR-10 validation accuracy')
+    plt.show(block=True)
+
+    w = best_svm.W[:-1, :]
+    w = w.reshape(32, 32, 3, 10)
+    w_min, w_max = np.min(w), np.max(w)
+    classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    for i in xrange(10):
+        plt.subplot(2, 5, i + 1)
+        wimg = 255.0 * (w[:, :, :, i].squeeze() - w_min) / (w_max - w_min)
+        plt.imshow(wimg.astype('uint8'))
+        plt.axis('off')
+        plt.title(classes[i])
+    plt.show()
+
